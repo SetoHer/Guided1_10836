a2ccf1aaef645c1a42cd46f419e51ab77b9bf0a7
SetoHer <seto.herlambang.23@gmail.com>
2022-09-15 14:21:08 +0700

    Guided1 10836

---
+++ app/src/main/java/com/example/view_binding_10836/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10836
+
+object TaskList {
+    val  taskList = listOf<Task> (
+        Task( "Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task( "Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha Mama Yaemiko tercinta / Keging Wangi"),
+        Task( "Chapter 3", "Ngopi", "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task( "Chapter 4", "Dimana Mama Ei?", "Buat Adu Mekanik Showcase Ayaka Pyro"),
+        Task( "Chapter 5", "Upode ke Tiktok", "Buat pamer ke view Showcase Ayaka Pyro C6 + Benner C6")
+    )
+
+}
